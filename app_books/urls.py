75e9f46e0fbf65365ab0e095194ce5845ea96aef
berdiyorova <rano.baxromovna@gmail.com>
2024-10-28 05:59:19 +0500

    Created UD views

--- app_books/urls.py
+++ app_books/urls.py
@@ -5,6 +5,8 @@ from app_books import views
 app_name = 'books'
 
 urlpatterns = [
-    path('authors/', views.author_list_create_view, name='author_list_create'),
+    path('authors/<int:pk>', views.author_detail, name='author_detail'),
+    path('<int:pk>', views.book_detail, name='book_detail'),
+    path('authors', views.author_list_create_view, name='author_list_create'),
     path('', views.book_list_create_view, name='book_list_create'),
 ]
--- app_books/views.py
+++ app_books/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
 from app_books.models import BookModel, AuthorModel
@@ -7,6 +8,11 @@ from app_books.serializers import BookModelSerializer, AuthorModelSerializer
 
 @api_view(['GET', 'POST'])
 def author_list_create_view(request):
+    """
+    Get authors list or add new author to database
+    :param request:
+    :return:
+    """
     if request.method == 'GET':
         authors = AuthorModel.objects.all()
         data = AuthorModelSerializer(authors, many=True).data
@@ -27,8 +33,69 @@ def author_list_create_view(request):
         )
 
 
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def author_detail(request, pk):
+    """
+    Get author by id and update or delete the author
+    :param request:
+    :param pk:
+    :return:
+    """
+    author = get_object_or_404(AuthorModel, pk=pk)
+
+    if request.method == 'GET':
+        data = AuthorModelSerializer(author).data
+        return Response(data=data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serialized_data = AuthorModelSerializer(author, data=request.data)
+        serialized_data.is_valid(raise_exception=True)
+
+        serialized_data.save()
+        return Response(
+            {
+                'success': True,
+                'message': 'Author is updated',
+                'data': serialized_data.data,
+            },
+            status=status.HTTP_200_OK
+        )
+
+    elif request.method == 'PATCH':
+        serialized_data = AuthorModelSerializer(author, data=request.data, partial=True)
+        serialized_data.is_valid(raise_exception=True)
+
+        serialized_data.save()
+        return Response(
+            {
+                'success': True,
+                'message': 'Author is partially updated',
+                'data': serialized_data.data,
+            },
+            status=status.HTTP_200_OK
+        )
+
+    elif request.method == 'DELETE':
+        author.delete()
+        return Response(
+            {
+                'success': True,
+                'message': 'Author is deleted',
+            },
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+
+# -----------------------------------------------------------------------------------------------------------
+
+
 @api_view(['GET', 'POST'])
 def book_list_create_view(request):
+    """
+    Get books list or create new book
+    :param request:
+    :return:
+    """
     if request.method == 'GET':
         books = BookModel.objects.all()
         data = BookModelSerializer(books, many=True).data
@@ -46,4 +113,57 @@ def book_list_create_view(request):
                 'data': serialized_data.data,
             },
             status=status.HTTP_201_CREATED
+        )
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def book_detail(request, pk):
+    """
+    Get book by id and update or delete the book
+    :param request:
+    :param pk:
+    :return:
+    """
+    book = get_object_or_404(BookModel, pk=pk)
+
+    if request.method == 'GET':
+        data = BookModelSerializer(book).data
+        return Response(data=data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serialized_data = BookModelSerializer(book, data=request.data)
+        serialized_data.is_valid(raise_exception=True)
+
+        serialized_data.save()
+        return Response(
+            {
+                'success': True,
+                'message': 'Book is updated',
+                'data': serialized_data.data,
+            },
+            status=status.HTTP_200_OK
+        )
+
+    elif request.method == 'PATCH':
+        serialized_data = BookModelSerializer(book, data=request.data, partial=True)
+        serialized_data.is_valid(raise_exception=True)
+
+        serialized_data.save()
+        return Response(
+            {
+                'success': True,
+                'message': 'Book is partially updated',
+                'data': serialized_data.data,
+            },
+            status=status.HTTP_200_OK
+        )
+
+    elif request.method == 'DELETE':
+        book.delete()
+        return Response(
+            {
+                'success': True,
+                'message': 'Book is deleted',
+            },
+            status=status.HTTP_204_NO_CONTENT
         )
